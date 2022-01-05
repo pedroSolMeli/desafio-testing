368dc8f6465cccd427369aff35041379f70c4c84
Pedro Sol <pedro.sol@mercadolivre.com>
2022-01-05 12:39:47 -0300

    adicionando create

--- src/main/java/com/mercadolibre/desafiotesting/service/LocalidadeService.java
+++ src/main/java/com/mercadolibre/desafiotesting/service/LocalidadeService.java
@@ -1,6 +1,8 @@
 package com.mercadolibre.desafiotesting.service;
 
 import com.mercadolibre.desafiotesting.model.Localidade;
+import com.mercadolibre.desafiotesting.repository.LocalidadeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,8 +10,22 @@ import java.util.List;
 @Service
 public class LocalidadeService {
 
+    @Autowired
+    LocalidadeRepository localidadeRepository;
+
     public List<Localidade> findAll(){
-        return null;
+        List<Localidade> result = localidadeRepository.findAll();
+        return result;
+    }
+
+    public Localidade findById(Long id){
+        Localidade result = localidadeRepository.findById(id).orElse(null);
+        return result;
+    }
+
+    public Localidade create(Localidade localidade){
+        Localidade result = localidadeRepository.save(localidade);
+        return result;
     }
 
 }
