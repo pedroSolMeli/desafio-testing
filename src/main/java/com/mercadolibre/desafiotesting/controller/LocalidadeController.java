35dccd4e266ae13ff507ba85c21c519016988343
renanmeli <renan.rsantana@mercadolivre.com>
2022-01-05 14:13:20 -0300

    Localidade Controller

--- src/main/java/com/mercadolibre/desafiotesting/controller/LocalidadeController.java
+++ src/main/java/com/mercadolibre/desafiotesting/controller/LocalidadeController.java
@@ -3,13 +3,10 @@ package com.mercadolibre.desafiotesting.controller;
 import com.mercadolibre.desafiotesting.model.Localidade;
 import com.mercadolibre.desafiotesting.service.LocalidadeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.LinkedHashSet;
 import java.util.List;
 
 @RestController
@@ -22,8 +19,35 @@ public class LocalidadeController {
     @GetMapping()
     public ResponseEntity<?> findAll(){
         List<Localidade> result = localidadeService.findAll();
-
         return ResponseEntity.ok(result);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id){
+        Localidade result = localidadeService.findById(id);
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping()
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public ResponseEntity<?> create(@RequestBody Localidade localidade){
+        Localidade result = localidadeService.create(localidade);
+        return new ResponseEntity<>(result, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.OK)
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Localidade localidade){
+        localidadeService.updateById(id, localidade);
+        return new ResponseEntity<>("Updated localidade id: " + id, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.OK)
+    public ResponseEntity<?> delete(@PathVariable Long id){
+        localidadeService.deleteById(id);
+        return new ResponseEntity<>("Deleted localidade id: " + id,HttpStatus.OK);
+    }
+
 
 }
