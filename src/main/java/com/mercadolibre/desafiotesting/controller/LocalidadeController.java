7d4ce121a76dd05777a41b17a36b23801b6db517
renanmeli <renan.rsantana@mercadolivre.com>
2022-01-05 12:25:41 -0300

    get

--- src/main/java/com/mercadolibre/desafiotesting/controller/LocalidadeController.java
+++ src/main/java/com/mercadolibre/desafiotesting/controller/LocalidadeController.java
@@ -1,7 +1,29 @@
 package com.mercadolibre.desafiotesting.controller;
 
+import com.mercadolibre.desafiotesting.model.Localidade;
+import com.mercadolibre.desafiotesting.service.LocalidadeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.LinkedHashSet;
+import java.util.List;
+
 @RestController
+@RequestMapping("/localidade")
 public class LocalidadeController {
+
+    @Autowired
+    LocalidadeService localidadeService;
+
+    @GetMapping()
+    public ResponseEntity<?> findAll(){
+        List<Localidade> result = localidadeService.findAll();
+
+        return ResponseEntity.ok(result);
+    }
+
 }
--- src/main/java/com/mercadolibre/desafiotesting/service/LocalidadeService.java
+++ src/main/java/com/mercadolibre/desafiotesting/service/LocalidadeService.java
@@ -1,7 +1,15 @@
 package com.mercadolibre.desafiotesting.service;
 
+import com.mercadolibre.desafiotesting.model.Localidade;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class LocalidadeService {
+
+    public List<Localidade> findAll(){
+        return null;
+    }
+
 }
