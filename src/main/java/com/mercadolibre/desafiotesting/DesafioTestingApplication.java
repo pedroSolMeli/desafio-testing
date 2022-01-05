9e909c29b5c139f16baf0cac1e4e294ae69c4186
Pedro Sol <pedro.sol@mercadolivre.com>
2022-01-05 10:04:41 -0300

    initial commit

---
+++ src/main/java/com/mercadolibre/desafiotesting/DesafioTestingApplication.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.desafiotesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mercadolibre/desafiotesting/DesafioTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.desafiotesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
